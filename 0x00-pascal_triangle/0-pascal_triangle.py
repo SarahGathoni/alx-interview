a1f8c6ed8dc2a92fde2b92838f7e20b86b96fe58
SarahGathoni <sarah.wanjiku.gathoni@gmail.com>
2023-07-12 09:05:59 +0000

    changes

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python3
-'''
-Returns list representing the Pascal's triangle of n
-'''
+""" a function that creates the Pascal's triangle"""
+
+
 def pascal_triangle(n):
-   
-    if n <= 0:
-        '''returns empty list if n <= 0'''
-        return []
-
-    triangle = [[1]]
-
+    """Pascal's triangle function"""
+    triangle_list = [[1]]
     for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
-        triangle.append(row)
-    return triangle
-    
+        row = [1] + [triangle_list[i-1][j] + triangle_list[i-1][j+1] for j in range(i-1)] + [1]
+        triangle_list.append(row)
+    return triangle_list if n > 0 else []    
